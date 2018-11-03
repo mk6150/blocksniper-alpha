674f4bfad65cb9ac307e78d1cd8add488de24eef
sniperkitty <mk6150@nyu.edu>
2018-11-03 02:01:26 +0000

    blocksniper-alpha-2

---
+++ dev/src/blocksniper-alpha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blocksniper_alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
